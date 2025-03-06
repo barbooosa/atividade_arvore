5bd4dbc215aead95f852d25cac1a8167ee41645c
barbooosa <jpmbarbosa48@gmail.com>
2025-03-06 12:33:09 -0300

    atividade de estudo

---
+++ 2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Função para realizar o BubbleSort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Troca os elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Função para imprimir o array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Lista original: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Lista ordenada: \n");
+    printArray(arr, n);
+
+    return 0;
+}
---
+++ 3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Troca os elementos
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+};
+
+// Função para imprimir o array
+void printArray(int arr[], int size) ;{
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+};
+
+int main() ;{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("Lista original: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Lista ordenada: \n");
+    printArray(arr, n);
+
+    return 0;
+}
